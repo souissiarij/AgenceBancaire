2b6cc3a1a11cc5e95df5b898f63f8c14d1d97049
NADAEB21 <153819993+NADAEB21@users.noreply.github.com>
2023-12-14 22:23:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,85 +4,89 @@
 
 
 int main() {
-    int option;
+    //variables de commutation
+    int option,x,a,y;
+    //tailles des tableaux initialisés a zero au ca ou l'utilisateur n'a pas saisie d'informations 
     int nbcli=0, nbcmpte=0, nbtrans=0;
+    //tableaux dynamiques d'adresses des structures   
     CLIENT **cli;
     COMPTEBANCAIRE **cmp;
     TRANSACTION **trans;
     CLIENT** reallocclient;
-    int x,a,ajout=0;
+    //nombre des clients a ajouter si l'utilisateur souhite ajouter des clients
+    int ajout=0;
+    TRANSACTION nouvelletranstaction;
 
     do {
-        printf("--------------------MENU PRINCIPAL-----------------\n");
+        printf("\n--------------------MENU PRINCIPAL-----------------\n");
         printf("Que voulez vous faire ?  \n");
         printf("tapez 1 pour effectuer un remplissage  \n");
-        printf("tapez 2 pour effectuer une transaction \n");
-        printf("tapez 3 afficher les informations des clients , comptes ou transactions \n ");
-        printf("tapez 4 pour modifier les informations d'un client  \n");
+        printf("tapez 2 afficher les informations des clients , comptes ou transactions \n ");
+        printf("tapez 3 pour modifier les informations d'un client  \n");
+        printf("tapez 4 pour pour effectuer une transaction \n");
         printf("taper 5 pour quitter \n");
-
         scanf("%d", &option);
         switch (option) {
             case 1:
-                printf("tapez 1 pour remplir le tableau des clients \n");
-                printf("tapez 2 pour remplir le tableau des comptes \n");
-                printf("tapez 3 pour remplir le tableau des transactions \n");
-                printf("taper 4 pour revenir au menu principal \n");
-                scanf("%d", &x);
-                switch (x) {
-                    case 1:
-                        printf("donner le nombre des clients");
-                        scanf("%d", &nbcli);
-                        // allocation de memoire
-                        allouerclient(&cli, nbcli);
-                        // remplissage tableau
-                        printf("**remplissage du tableau**\n");
-                        remplirtabcli(&cli, nbcli);
-                        printf("\nvoulez vous ajoutez des clients au tableau? :tapez 1 pou oui 2 pour non");
-                        scanf("%d",&a);
-                        switch(a){
-                            case 1:
-                            printf("\ncombien de cases voulez vous ajoutez?");
-                            scanf("%d",&ajout);
-                            realloclient(&cli,nbcli,ajout);
-                            tabcliajout(&cli,nbcli,ajout);
+                do{
+                    printf("tapez 1 pour remplir le tableau des clients \n");
+                    printf("tapez 2 pour remplir le tableau des comptes \n");
+                    printf("tapez 3 pour remplir le tableau des transactions \n");
+                    printf("taper 4 pour revenir au menu principal \n");
+                    scanf("%d", &x);
+                    switch (x) {
+                        case 1:
+                            printf("donner le nombre des clients");
+                            scanf("%d", &nbcli);
+                            // allocation de memoire
+                            allouerclient(&cli, nbcli);
+                            // remplissage tableau
+                            printf("**remplissage du tableau**\n");
+                            remplirtabcli(&cli, nbcli);
+                            printf("\nvoulez vous ajoutez des clients au tableau? :tapez 1 pou oui 2 pour non");
+                            scanf("%d",&a);
+                            switch(a){
+                                case 1:
+                                printf("\ncombien de cases voulez vous ajoutez?");
+                                scanf("%d",&ajout);
+                                realloclient(&cli,nbcli,ajout);
+                                tabcliajout(&cli,nbcli,ajout);
+                                break;
+                                case 2:
+                                break;
+                            }
                             break;
-                            case 2:
+                        case 2:
+                            printf("donner le nombre des comptes ");
+                            scanf("%d", &nbcmpte);
+                            // allocation de memoire
+                            allouercompte(&cmp, nbcmpte);
+                            // remplissage tableau
+                            printf("**remplissage du tableau**\n");
+                            remplirtabcomptes(&cmp, nbcmpte);
                             break;
-                        }
-                        break;
-                    case 2:
-                        printf("donner le nombre des comptes ");
-                        scanf("%d", &nbcmpte);
-                        // allocation de memoire
-                        allouercompte(&cmp, nbcmpte);
-                        // remplissage tableau
-                        printf("**remplissage du tableau**\n");
-                        remplirtabcomptes(&cmp, nbcmpte);
-                        break;
-                    case 3:
-                        printf("donner le nombre des transactions ");
-                        scanf("%d", &nbtrans);
-                        // allocation de memoire
-                        allouertransaction(&trans, nbtrans);
-                        // remplissage tableau
-                        printf("**remplissage du tableau**\n");
-                        remplirtabtrans(&trans, nbtrans);
-                        break;
-                    case 4:
-                        break;
-                }
-                break;
-            case 3:
+                        case 3:
+                            printf("donner le nombre des transactions ");
+                            scanf("%d", &nbtrans);
+                            // allocation de memoire
+                            allouertransaction(&trans, nbtrans);
+                           // remplissage tableau
+                            printf("**remplissage du tableau**\n");
+                            remplirtabtrans(&trans, nbtrans);
+                            break;
+                    }                   
+                }while(x!=4); 
+                break;         
+            case 2:
                 printf("tapez 1 pour afficher le tableau des clients \n");
                 printf("tapez 2 pour afficher le tableau des comptes \n");
                 printf("tapez 3 pour afficher le tableau des transactions \n");
                 printf("taper 4 pour revenir au menu principal \n");
-                scanf("%d", &x);
-                switch (x)
+                scanf("%d", &y);
+                switch (y)
                 {
                 case 1:
-
+             //verifier que le tableau des clients n'est pas vide
                     if((nbcli+ajout)>0){
                         printf("\n**affichage du tableau clients**");
                         affichertabcli(cli,(nbcli+ajout));
@@ -94,9 +98,10 @@ int main() {
                     break;
                 
                 case 2:
+                //verifier que le tableau des comptes n'est pas vide
                     if((nbcmpte)>0){
                         printf("\n**affichage du tableau comptes**");
-                        affichertabcli(cli,(nbcmpte));
+                        affichertabcomptes(cmp,(nbcmpte));
 
                     }
                     else{
@@ -104,30 +109,32 @@ int main() {
                     }
                     break;
                 case 3:
+                //verifier que le tableau des transactions n'est pas vide
                     if((nbtrans)>0){
                         printf("\n**affichage du tableau transactions**");
-                        affichertabcli(cli,(nbtrans));
+                        affichertabtrans(trans,(nbtrans));
                     }
                     else{
                         printf("\nvous n'avez  encore saisi aucune transaction");
                     }
-                    break;
-                
+                    break;                                
                 case 4 :
                 break;  
-
-
+                }                
+                break;
+            case 3:
+            //verifie que le tableau des clients n'est pas vide
+                if((nbcli+ajout)>0){
+                    modifclient(&cli,(nbcli+ajout));
                 }
-                
-                break;
+                else{
+                    printf("\nvous n'avez  encore saisi aucun client");
+                }
+                break; 
             case 4:
-                printf("444\n");
-                break;
+                effectuertransaction(&nouvelletranstaction);
         }
-
     } while (option != 5);
-
-    printf("--------fin-----\n");
-
+    printf("\n------Fin------");
     return 0;
 }
